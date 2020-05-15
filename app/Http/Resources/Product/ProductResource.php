be870ddaa3c1ee1ca46870b1536e48514a21dca6
Chiysom <chisom_ogbuagu@yahoo.com>
2020-05-15 22:10:04 +0100

    Product collection resource

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -21,7 +21,7 @@ class ProductResource extends JsonResource
             'stock' => $this->stock > 0 ? $this->stock : 'Out of Stock!',
             'discount' => $this->discount,
             'totalPrice' => round((1 - ($this->discount/100)) * $this->discount,2),
-            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count()) : 'No rating yet',
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
             'href' => [
                 'reviews' => route('reviews.index',$this->id)
             ]
